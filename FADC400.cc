69cdbff839bbeb64198cad103fc0babd2b4ee9cc
Genie Jhang <geniejhang@majimak.com>
2013-10-22 21:53:10 +0900

    - GUI items are added -- Button for same settings on all the FADC400 modules -- Tab frame for two FADC400 modules

--- FADC400.cc
+++ FADC400.cc
@@ -18,9 +18,48 @@
 ClassImp(FADC400);
 
 FADC400::FADC400(const TGWindow *window, UInt_t width, UInt_t height)
-:TGMainFrame(window, width, height)
+:TGMainFrame(window, width, height, kMainFrame)
 {
+	FILE *fp = fopen("DEBUG", "r");
+	if (fp) {
+		fclose(fp);
+		fIsDebug = kTRUE;
+	} else  
+		fIsDebug = kFALSE;
+
   SetWindowName("FADC400 Controller");
+  SetName("fMainWindow");
+  SetLayoutBroken(kTRUE);
+
+  // == Start of Module Frame =====================================================
+  fModuleFrame = new TGGroupFrame(this, "Module");
+  fModuleFrame -> SetLayoutBroken(kTRUE);
+
+  fSameModuleSettingButton = new TGCheckButton(fModuleFrame, "Use the same setting for all modules");
+  fSameModuleSettingButton -> Connect("Toggled(Bool_t)", "FADC400", this, "SetSameModuleSetting(Bool_t)");
+  fSameModuleSettingButton -> Move(10, 20);
+
+  Int_t moduleFrameHMargin = 5;
+  Int_t moduleFrameVMargin = 5;
+  Int_t moduleFrameWidth = width - 2*moduleFrameHMargin;
+  Int_t moduleFrameHeight = height - 2*moduleFrameVMargin;
+  fModuleFrame -> MoveResize(moduleFrameHMargin, moduleFrameVMargin, moduleFrameWidth, moduleFrameHeight);
+
+  fModuleTab = new TGTab(fModuleFrame);
+  fFADC1 = fModuleTab -> AddTab("FADC400 Module 1");
+  fFADC2 = fModuleTab -> AddTab("FADC400 Module 2");
+
+  fModuleFrame -> AddFrame(fModuleTab, new TGLayoutHints(kLHintsLeft|kLHintsTop));
+  Int_t moduleTabHMargin = 10;
+  Int_t moduleTabVMargin = 10;
+  Int_t moduleTabWidth = moduleFrameWidth - 2*moduleTabHMargin;
+  Int_t moduleTabHeight = moduleFrameHeight - 9*moduleTabVMargin;
+  fModuleTab -> MoveResize(10, 45, moduleTabWidth, moduleTabHeight);
+  AddFrame(fModuleFrame, new TGLayoutHints(kLHintsLeft|kLHintsTop));
+
+  // == End of Module Frame =======================================================
+
+  MapSubwindows();
   MapWindow();
 }
 
@@ -29,10 +68,21 @@ FADC400::~FADC400()
   Cleanup();
 }
 
+void FADC400::SetSameModuleSetting(Bool_t isSet)
+{
+  if (fIsDebug) {
+    cout << "==============================" << endl;
+    cout << " SetSameModuleSetting is " << ( isSet ? "On!" : "Off!" ) << endl;
+    cout << "==============================" << endl;
+  }
+
+  // Some code will be here
+}
+
 Int_t main(int argc, char **argv)
 {
 	TApplication theApp("FADC400GUI", &argc, argv);
-	new FADC400(gClient -> GetRoot(), 620, 500);
+	new FADC400(gClient -> GetRoot(), 800, 1000);
 	theApp.Run();
 
 	return 0;
--- FADC400.hh
+++ FADC400.hh
@@ -11,8 +11,10 @@
 #ifndef _FADC400_H_
 #define _FADC400_H_
 
-#include "TGFrame.h"
 #include "TGWindow.h"
+#include "TGFrame.h"
+#include "TGTab.h"
+#include "TGButton.h"
 
 class FADC400 : public TGMainFrame
 {
@@ -20,7 +22,17 @@ class FADC400 : public TGMainFrame
     FADC400(const TGWindow *, UInt_t, UInt_t);
     virtual ~FADC400();
 
+    void SetSameModuleSetting(Bool_t);
+
   private:
+    Bool_t fIsDebug;
+
+    TGGroupFrame *fModuleFrame;
+    TGButton *fSameModuleSettingButton;
+    TGTab *fModuleTab;
+    TGCompositeFrame *fFADC1;
+    TGCompositeFrame *fFADC2;
+
 
   ClassDef(FADC400, 1);
 };
