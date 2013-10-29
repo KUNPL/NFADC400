84b501c8b44a38c706f878b4d1a592ba172efc29
Genie Jhang <geniejhang@majimak.com>
2013-10-29 18:41:59 +0900

    - Start to write Process class. - Don't forget --no-ff option when merge

---
+++ FADC400Process.cc
@@ -0,0 +1,13 @@
+/////////////////////////////////////
+//                                 //
+//       FADC400 module GUI        //
+//    Data Taking Process Class    //
+//                                 //
+//  Author: Genie Jhang            //
+// Contact: geniejhang@majimak.com //
+//    Date: 2013. 10. 29           //
+//                                 //
+/////////////////////////////////////
+
+#include "FADC400Process.hh"
+
---
+++ FADC400Process.hh
@@ -0,0 +1,22 @@
+/////////////////////////////////////
+//                                 //
+//       FADC400 module GUI        //
+//    Data Taking Process Class    //
+//                                 //
+//  Author: Genie Jhang            //
+// Contact: geniejhang@majimak.com //
+//    Date: 2013. 10. 29           //
+//                                 //
+/////////////////////////////////////
+
+#ifndef __FADC400PROCESS_H_
+#define __FADC400PROCESS_H_
+
+class FADC400Process 
+{
+  public:
+
+  private:
+};
+
+#endif
