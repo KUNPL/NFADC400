b88bc657fb0ca348adca1626723d1f02f6fcb579
Genie Jhang <geniejhang@majimak.com>
2013-11-01 19:19:31 +0900

    - Branch for development of shared library for analysis

---
+++ analysis/FADC400Constants.hh
@@ -0,0 +1,15 @@
+#ifndef _FADC400CONSTANTS_H_
+#define _FADC400CONSTANTS_H_
+
+#define RL1   0x400  
+#define RL2   0x800  
+#define RL4   0x1000 
+#define RL8   0x2000 
+#define RL16  0x4000 
+#define RL32  0x8000 
+#define RL64  0x10000
+#define RL128 0x20000
+#define RL256 0x40000
+#define RL512 0x80000
+
+#endif
